b388873664c4f6a46f69e16f4797dd9a02327c88
prateekinstillion <Prateek Madaan>
2019-07-08 12:49:43 +0530

    added the local storage to the app

--- src/contexts/BookContext.js
+++ src/contexts/BookContext.js
@@ -1,9 +1,17 @@
-import React, { createContext, useReducer } from "react";
+import React, { createContext, useReducer, useEffect } from "react";
 import bookReducer from "../reducers/bookReducer";
 export const BookContext = createContext();
 
 const BookContextProvider = props => {
-  const [books, dispatch] = useReducer(bookReducer, []);
+  const [books, dispatch] = useReducer(bookReducer, [], () => {
+    const books = localStorage.books;
+    return books ? JSON.parse(books) : [];
+  });
+  // maintain the list of the books inside the local storage
+  useEffect(() => {
+    localStorage.setItem("books", JSON.stringify(books));
+  }, [books]);
+
   return (
     <BookContext.Provider value={{ books, dispatch }}>
       {props.children}
